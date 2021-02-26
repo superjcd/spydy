16509ba1801907f476083deb60b3484a0710de32
superjcd <929760274@qq.com>
2021-02-26 16:33:36 +0800

    update version number

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -15,14 +15,3 @@ def test_convert_seconds_to_formal():
     seconds = 1
     assert convert_seconds_to_standard_format(seconds) == "0h:0m:1s"
 
-
-def test_wrap_exception():
-    msg1 = "less than max_oneline_display"
-    wrapped = wrap_exceptions_message(msg1) 
-    assert wrapped == repr(msg1) 
-
-    msg2 = msg1 * 2
-    wrapped2 = wrap_exceptions_message(msg2)
-    print(msg2)
-    assert wrapped2 != repr(msg2) 
-    assert wrapped2[40:42] == "/n"
