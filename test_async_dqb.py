1422063031d8ab22f58fd74fc96c3c621b6e10f7
蒋超迪 <jiangcd@chinaoly.com>
2021-04-16 22:56:12 +0800

    add logic for async db insert

--- test_async_dqb.py
+++ test_async_dqb.py
@@ -1,5 +1,6 @@
 import aiosqlite
 import asyncio
+from collections import Iterable
 from sqlalchemy import text, MetaData, Table
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,21 +12,27 @@ engine = create_async_engine(
         connection_url, echo=False,
     )
 
-
+items1 = {'categories': 3, 'editors': "uuuu", 'languages': 8, 'sites': 10}
+items = {}
 async def async_main():  
     async with engine.begin() as conn:
-        items1 = {'categories': 3, 'editors': 6, 'languages': 8, 'sites': 10}
-        sql = prepare_text_for_multiple_data(items=items1, table_name="stats")
+        
+        sql = prepare_sql_for_execute(items, "stats")
+        # sql = prepare_text_for_multiple_data(items=items1, table_name="stats")
         # print(sql)
         await conn.execute(
-             text(sql)
+             text(sql), [items1]
              )
 
-
-def prepare_text_for_multiple_data(items, table_name):
-    if type(items) == dict:
-        sql = f"insert into {table_name} ({','.join(list(items.keys()))}) values ({','.join(list(items.values()))})"
+# 假设数据位空， 我应该应该返回一个标志， 然后不去执行！
+def prepare_sql_for_execute(items, table_name):
+    if isinstance(items, dict):
+        item_keys = items.keys()
+        col_names = ','.join(item_keys)
+        placeholders = ','.join([':' + col for col in item_keys])
+        sql = f"INSERT INTO {table_name} ({col_names}) VALUES ({placeholders})"
         return sql
 
 
 asyncio.run(async_main())
+# print(prepare_text_for_multiple_data(items1, "stats"))
